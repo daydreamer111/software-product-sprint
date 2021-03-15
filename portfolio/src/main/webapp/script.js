e68309b467f3ae6e31d19b81559589cceca69b70
Jackie <1jackiemoni@gmail.com>
2021-03-15 04:44:03 +0000

    Adding translate feature and header

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,20 +1,4 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * Adds a random book to the page.
- */
+//Adds a random book to the page.
 function addRandomBook() {
   const books =
       ['The Games Maker by Pablo de Santis', 'Carve the Mark by Veronica Roth', 'Cinder by Marissa Meyer', 'Warcross by Marie Lu', 'Throne of Glass by Sarah J. Mass', 'Dragon Rider by Cornelia Funke', 'Eragon by Cristopher Paolini', 'Eon: Dragoneye Reborn by Alison Goodman', 'I am Number Four by Pittacus Lore'];
@@ -35,4 +19,24 @@ async function showRandomSong(){
     const textResp = textRespObj.song + " - " + textRespObj.artist;
     const song = document.getElementById('song-container');
     song.innerText = textResp;
+}
+
+function requestTranslation() {
+    const text = document.getElementById('content').innerHTML;
+    const languageCode = document.getElementById('language').value;
+
+    const resultContainer = document.getElementById('content');
+    resultContainer.innerText = 'Loading...';
+
+    const params = new URLSearchParams();
+    params.append('text', text);
+    params.append('languageCode', languageCode);
+
+    fetch('/translate', {
+        method: 'POST',
+        body: params
+    }).then(response => response.text())
+    .then((translatedMessage) => {
+        resultContainer.innerHTML = translatedMessage;
+    });
 }
